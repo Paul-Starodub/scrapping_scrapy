d811485b20b9ef2234b8c7d90ba4bf2b4c3b622c
Paul Starodub <amorallex@gmail.com>
2023-06-15 20:47:37 +0300

    Scrapy

---
+++ requests_bs4/parse.py
@@ -0,0 +1,140 @@
+import csv
+import logging
+import sys
+from dataclasses import dataclass, fields, astuple
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+BASE_URL = "https://webscraper.io/"
+LAPTOP_URL = urljoin(
+    BASE_URL, "test-sites/e-commerce/static/computers/laptops"
+)
+
+PRODUCTS_OUTPUT_CSV_PATH = "products.csv"
+
+_driver: WebDriver | None = None
+
+
+def get_driver() -> WebDriver:
+    return _driver
+
+
+def set_driver(new_driver: WebDriver) -> None:
+    global _driver
+    _driver = new_driver
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)8s]:  %(message)s",
+    handlers=[
+        logging.FileHandler("parser.log"),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
+
+
+@dataclass
+class Product:
+    title: str
+    description: str
+    price: float
+    rating: int
+    num_of_reviews: int
+    additional_info: dict
+
+
+PRODUCTS_FIELDS = [field.name for field in fields(Product)]
+
+
+def parse_hdd_block_prices(product_soup: BeautifulSoup) -> dict[str, float]:
+    detailed_url = urljoin(BASE_URL, product_soup.select_one(".title")["href"])
+    driver = get_driver()
+    driver.get(detailed_url)
+    swatches = driver.find_element(By.CLASS_NAME, "swatches")
+    buttons = swatches.find_elements(By.TAG_NAME, "button")
+
+    prices = {}
+
+    for button in buttons:
+        if not button.get_property("disabled"):
+            button.click()
+            prices[button.get_property("value")] = float(
+                driver.find_element(By.CLASS_NAME, "price").text.replace(
+                    "$", ""
+                )
+            )
+
+    return prices
+
+
+def parse_single_product(product_soup: BeautifulSoup) -> Product:
+    hdd_prices = parse_hdd_block_prices(product_soup)
+    return Product(
+        title=product_soup.select_one(".title")["title"],
+        description=product_soup.select_one(".description").text,
+        price=float(product_soup.select_one(".price").text.replace("$", "")),
+        rating=int(product_soup.select_one("p[data-rating]")["data-rating"]),
+        num_of_reviews=int(
+            product_soup.select_one(".ratings > p.pull-right").text.split()[0]
+        ),
+        additional_info={"hdd_prices": hdd_prices},
+    )
+
+
+def get_num_pages(page_soup: BeautifulSoup) -> int:
+    pagination = page_soup.select_one(".pagination")
+
+    if pagination is None:
+        return 1
+
+    return int(pagination.select("li")[-2].text)
+
+
+def get_single_page_products(page_soup: BeautifulSoup) -> [Product]:
+    products = page_soup.select(".thumbnail")
+    return [parse_single_product(product_soup) for product_soup in products]
+
+
+def get_laptop_products() -> [Product]:
+    logging.info("Start parsing laptops")
+    page = requests.get(LAPTOP_URL).content
+    first_page_soup = BeautifulSoup(page, "html.parser")
+
+    # get num of pages
+    num_pages = get_num_pages(first_page_soup)
+
+    all_products = get_single_page_products(first_page_soup)
+
+    # iterate on pages & get all products on single page
+    for page_num in range(2, num_pages + 1):
+        logging.info(f"Start parsing page #{page_num}")
+        page = requests.get(LAPTOP_URL, {"page": page_num}).content
+        soup = BeautifulSoup(page, "html.parser")
+        all_products.extend(get_single_page_products(soup))
+        break  # remove this - just for debugging
+
+    return all_products
+
+
+def write_products_to_csv(products: [Product]) -> None:
+    with open(PRODUCTS_OUTPUT_CSV_PATH, "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(PRODUCTS_FIELDS)
+        writer.writerows([astuple(product) for product in products])
+
+
+def main():
+    with webdriver.Chrome() as new_driver:
+        set_driver(new_driver)
+        products = get_laptop_products()
+        write_products_to_csv(products)
+
+
+if __name__ == "__main__":
+    main()
